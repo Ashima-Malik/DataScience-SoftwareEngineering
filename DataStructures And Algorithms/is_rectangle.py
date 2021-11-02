2eca4e15146c2d638a193c418069aee0f227481a
Ashima-Malik <54690347+Ashima-Malik@users.noreply.github.com>
2021-11-02 01:04:04 -0700

    Add files via upload

---
+++ DataStructures And Algorithms/is_rectangle.py
@@ -0,0 +1,13 @@
+def is_rectangle(A,B,C,D):
+    _dict = {}
+    if (A==B or A==C or A==D):
+        _dict[A]=2
+    if (B == C or B==D):
+        _dict[B]=2
+    if C==D:
+        _dict[C]=2
+    if len(_dict) == 2:
+        return True
+    return False
+
+print(is_rectangle(1,2,3,4))
